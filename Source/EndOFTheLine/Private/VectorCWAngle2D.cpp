ebec91806ed1f6efd7ab353fb818c06dfed7ae08
Jesper Persson <eppej33@live.se>
2020-04-07 17:45:40 +0200

    Gun 1 lock on system, done

---
+++ Source/EndOFTheLine/Private/VectorCWAngle2D.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VectorCWAngle2D.h"
+#include "Kismet/KismetMathLibrary.h"
+
+float UVectorCWAngle2D::VectorCWAngle2D(FVector a, FVector b)
+{
+   FVector2D vecA;
+   vecA.X = a.X; vecA.Y = a.Y;
+   vecA.Normalize();
+
+   FVector2D vecB;
+   vecB.X = b.X; vecB.Y = b.Y;
+   vecB.Normalize();
+
+   float dotProduct = ( vecA.X * vecB.X ) + ( vecA.Y * vecB.Y );
+   float determinant = ( vecA.Y * vecB.X ) - ( vecA.X * vecB.Y );
+
+   return atan2f(determinant, dotProduct);
+}
+
+float UVectorCWAngle2D::SwitchTargetAngleScore(float angle)
+{
+   if (angle < 0)
+   {
+      angle += 6;
+   }
+   return angle;
+}
---
+++ Source/EndOFTheLine/Public/VectorCWAngle2D.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "VectorCWAngle2D.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENDOFTHELINE_API UVectorCWAngle2D : public UBlueprintFunctionLibrary
+{
+   GENERATED_BODY()
+
+   UFUNCTION(BlueprintPure, Category = "Lock-On")
+   static float VectorCWAngle2D(FVector a, FVector b);
+
+   UFUNCTION(BlueprintPure, Category = "Lock-On")
+   static float SwitchTargetAngleScore(float angle);
+};
+
